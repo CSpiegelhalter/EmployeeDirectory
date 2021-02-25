10148a68bdc78230ec186fe65cae39daab79c77f
CSpiegelhalter <spiegelhaltercurt@gmail.com>
2021-02-25 17:46:18 -0600

    Added onmount function. Started render

--- src/components/DataArea.js
+++ src/components/DataArea.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import DataTable from './DataTable';
 import Nav from './Navbar';
 import API from '../util/API';
+import Navbar from './Navbar';
 
 export default class DataArea extends Component {
 
@@ -18,4 +19,25 @@ export default class DataArea extends Component {
         {name: "Email", width: "15%"},
         {name: "DOB", width: "15%"}
     ]
+
+
+
+
+    componentDidMount() {
+        API.getUsers().then(results => {
+            this.setState({
+                users: results.data.results,
+                filteredUsers: results.data.results
+            })
+        })
+    }
+
+    render() {
+        return(
+            <div>
+                <Navbar handleSearchChange={this.handleSearchChange} />
+                
+            </div>
+        )
+    }
 }
