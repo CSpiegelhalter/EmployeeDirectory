7ea2c0b0b4c062adca647b7d47405a1f654a761a
Curt Spiegelhalter <i16dcurt@gmail.com>
2021-02-25 19:46:40 -0600

    Added map function for returned user data to be displayed in a table

--- src/components/DataBody.js
+++ src/components/DataBody.js
@@ -1,9 +1,41 @@
 import React from 'react';
 
 function  DataBody({ users }) {
+
+    function formatDate(date) {
+        
+    }
     return(
         <tbody>
-            
+            {users.map(({ login, name, picture, phone, email, dob }) => {
+                return(
+                <tr  key={login.uuid}>
+                    <td>
+                        <img
+                        src={picture.medium}
+                        alt={name.first + " " + name.last + "'s profile picture"}
+                        />
+                    </td>
+                    <td>
+                        {name.fisrt} {name.last}
+                    </td>
+                    <td>
+                        {phone}
+                    </td>
+                    <td>
+                        <a href={"send email to: " + email} target="__blank">
+                            {email}
+                        </a>
+                    </td>
+                    <td>
+                        {formatDate(dob.date)}
+                    </td>
+                </tr>
+                )
+            })
+            }
         </tbody>
     )
 }
+
+export default DataBody;
