e59a066f9f1539590bd6fd34c1d4b78a938d1e55
Curt Spiegelhalter <i16dcurt@gmail.com>
2021-02-25 20:16:52 -0600

    Fixing input change functions

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import Header from "./header"
 
-export default class Navbar extends Component {
-    render() {
-        return (
-            <div>
-                <Header />
-            </div>
-        )
-    }
+function Search(props) {
+    return (
+        <div>
+            
+        </div>
+    )
 }
