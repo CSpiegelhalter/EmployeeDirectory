dcba14582b7ef89a0a491d526178dc3c60acc0ab
Curt Spiegelhalter <i16dcurt@gmail.com>
2021-02-25 19:50:04 -0600

    Added date format function

--- src/components/DataBody.js
+++ src/components/DataBody.js
@@ -3,7 +3,13 @@ import React from 'react';
 function  DataBody({ users }) {
 
     function formatDate(date) {
-        
+        const dateArray = date.split("-")
+        const year = dateArray[0]
+        const month = dateArray[1]
+        const dayArray = dateArray[2].split("T")
+        const day = dayArray[0]
+        const newDate = [month, day, year].join("-")
+        return newDate;
     }
     return(
         <tbody>
