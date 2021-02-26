c915c01fe7c9bb6c75c6a6fca6ef6ad24f26268d
Curt Spiegelhalter <i16dcurt@gmail.com>
2021-02-25 20:21:29 -0600

    Fixed navbar search functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,17 @@
-import React, { Component } from 'react';
-import Header from "./header"
+import React from 'react';
 
-function Search(props) {
+
+function Navbar(props) {
     return (
         <div>
-            
+            <input
+                value={props.filteredUsers}
+                onChange={props.handleSearchChange}
+                type="text"
+                placeholder="Search employees"
+            />
         </div>
     )
 }
+
+export default Navbar;
